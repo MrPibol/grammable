8b5cfb3dffbe73e80a0534c678fe1c9680265adc
MrPibol <mr.jp.torres@gmail.com>
2018-07-12 22:08:27 +0000

    3. Creating Posts and Users. 7. Posting grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe GramsController, type: :controller do
             expect(response).to have_http_status(:success)
         end
     end
+
+    describe "gram#new action" do
+        it "shoould successfully show the new form" do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "gram#create action" do
+        it "should successfully save the message in the database" do
+            post :create, params: { gram: {message: 'Hello!'} }
+        end
+    end
 end
